35c3cc13ab120a747278423b544fad5fda83804d
michael-edgar <michael.edgar@students.ittralee.ie>
2020-01-24 11:38:27 +0000

    Lab 2

---
+++ TestApp/TestApp/Assets/Scripts/Controllable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controllable : MonoBehaviour
+{
+    [SerializeField]
+    private bool bounce = false;
+    private Material _material;
+    private int _direction = 1;
+
+    public bool GetBounce()
+    {
+        return bounce;
+    }
+
+    void Start()
+    {
+        if (GetComponent <Renderer>() && GetComponent <Renderer>().material)
+        {
+            _material = GetComponent<Renderer>().material;
+        }
+    }
+    
+    public void BounceCube()
+    {
+        gameObject.transform.position += Vector3.up * _direction;
+            
+        if (gameObject.transform.position.y >= 6.0f)
+        {
+            _direction = -1;
+        }
+            
+        else if (gameObject.transform.position.y <= -4.0f)
+        {
+            _direction = 1;
+        }
+    }
+
+    public void ChangeColour(Color newColour)
+    {
+        _material.color = newColour;
+    }
+}
---
+++ TestApp/TestApp/Assets/Scripts/TouchCube.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Timers;
+using UnityEngine;
+
+public class TouchCube : MonoBehaviour
+{
+    
+    private Ray _currentRay;
+    private Camera _myCamera;
+    private bool _hasTouchMoved;
+    private Timer _touchTimer;
+
+    private void Start()
+    {
+        _myCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            TouchingSomething();
+        }
+    }
+
+    private void TouchingSomething()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            _currentRay = _myCamera.ScreenPointToRay(touch.position);
+            Debug.DrawRay(_currentRay.origin, _currentRay.direction * 100, Color.blue);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(_currentRay, out hitInfo))
+            {
+                Controllable controllable = hitInfo.transform.GetComponent<Controllable>();
+                if (controllable.GetBounce()) { controllable.BounceCube(); }
+                else { CubeColour(controllable); }
+            }
+        }
+    }
+
+    void CubeColour(Controllable controllable)
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                {
+                    controllable.ChangeColour(Color.black);
+                    break;
+                }
+
+                case TouchPhase.Stationary:
+                {
+                    controllable.ChangeColour(Color.blue);
+                    break;
+                }
+
+                case TouchPhase.Moved:
+                {
+                    controllable.ChangeColour(Color.red);
+                    break;
+                }
+
+                case TouchPhase.Ended:
+                {
+                    controllable.ChangeColour(Color.green);
+                    break;
+                }
+
+                case TouchPhase.Canceled:
+                {
+                    controllable.ChangeColour(Color.yellow);
+                    break;
+                }
+                
+                default:
+                {
+                    Debug.LogError("This should never happen ever");
+                    break;
+                }
+            }
+        }
+    }
+}
