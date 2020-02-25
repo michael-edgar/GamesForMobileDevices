5b05428fd7fc2a12440f49008c9e18d67e4e1d93
michael-edgar <michael.edgar@students.ittralee.ie>
2020-02-25 09:48:00 +0000

    Started Second Assignment

---
+++ TestApp/Advertising Manager/Assets/Scripts/AdManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdManager : MonoBehaviour
+{
+    [SerializeField] private string gameId;
+    [SerializeField] private bool testMode = true;
+    [SerializeField] private bool enablePerPlacementLoad;
+    
+    public void MethodDump()
+    {
+        //Initialize(string gameId, bool testMode, bool enablePerPlacementLoad)
+    }
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
