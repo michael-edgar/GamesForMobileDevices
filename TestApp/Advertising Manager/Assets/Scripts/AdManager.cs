90d5a767ee8e1ff49cb50a8b000a88ad33b6e91a
michael-edgar <michael.edgar@students.ittralee.ie>
2020-03-19 10:53:24 +0000

    AdMob Finished

--- TestApp/Advertising Manager/Assets/Scripts/AdManager.cs
+++ TestApp/Advertising Manager/Assets/Scripts/AdManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using GoogleMobileAds.Api;
+using UnityEngine;
 using UnityEngine.Advertisements;
 using UnityEngine.UI;
 
@@ -8,13 +10,21 @@ public class AdManager : MonoBehaviour, IUnityAdsListener
     [SerializeField] private bool enablePerPlacementLoad = false;
     [SerializeField] private bool isPlatformAndroid = true;
     [SerializeField] private int numberOfCoins = 0;
+    [SerializeField] private int numberOfTokens = 0;
     [SerializeField] private Text textBox;
+    private RewardBasedVideoAd _adMobRewardedVideoAd;
+    private InterstitialAd _adMobInterstitialAd;
+    private BannerView _adMobBannerView;
     private const string TextNumberOfCoins = "The player has earned ";
-    private const string GameIdAndroid = "3483587";
-    private const string GameIdApple = "3483586";
-    private const string RewardedVideoPlacementId = "rewardedVideo";
-    private const string InterstitialPlacementId = "video";
-    private const string MenuBannerPlacementId = "MenuBanner";
+    private const string AdMobGameIdAndroid = "ca-app-pub-1272408203130077~9243552470";
+    private const string UnityGameIdAndroid = "3483587";
+    private const string UnityGameIdApple = "3483586";
+    private const string AdMobRewardedVideoAdId = "ca-app-pub-3940256099942544/5224354917";
+    private const string AdMobInterstitialAdId = "ca-app-pub-3940256099942544/1033173712";
+    private const string AdMobMenuBannerAdId = "ca-app-pub-3940256099942544/6300978111";
+    private const string UnityRewardedVideoPlacementId = "rewardedVideo";
+    private const string UnityInterstitialPlacementId = "video";
+    private const string UnityMenuBannerPlacementId = "MenuBanner";
 
     public void OnUnityAdsReady (string placementId) {
         // Causes ad to constantly play 
@@ -41,42 +51,157 @@ public class AdManager : MonoBehaviour, IUnityAdsListener
         }
     }
 
-    public void EnableRewardedVideoAd()
+    private void HandleRewardBasedVideoLoaded(object sender, EventArgs args)
     {
-        Advertisement.Load(RewardedVideoPlacementId);
-        Advertisement.Show(RewardedVideoPlacementId);
+        print("HandleRewardBasedVideoLoaded event received");
+        _adMobRewardedVideoAd.Show();
+    }
+
+    private void HandleRewardBasedVideoFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        print("HandleRewardBasedVideoFailedToLoad event received with message: " + args.Message);    }
+
+    private void HandleRewardBasedVideoOpened(object sender, EventArgs args)
+    {
+        print("HandleRewardBasedVideoOpened event received");
+    }
+
+    private void HandleRewardBasedVideoStarted(object sender, EventArgs args)
+    {
+        print("HandleRewardBasedVideoStarted event received");
     }
     
-    public void EnableInterstitialAd()
+    public void HandleRewardBasedVideoRewarded(object sender, Reward args)
     {
-        Advertisement.Load(InterstitialPlacementId);
-        Advertisement.Show(InterstitialPlacementId);
+        string type = args.Type;
+        double amount = args.Amount;
+        print("HandleRewardBasedVideoRewarded event received for " + amount + " " + type);
+        numberOfTokens++;
+    }
+
+    private void HandleRewardBasedVideoClosed(object sender, EventArgs args)
+    {
+        print("HandleRewardBasedVideoClosed event received");
+    }
+
+    private void HandleRewardBasedVideoLeftApplication(object sender, EventArgs args)
+    {
+        print("HandleRewardBasedVideoLeftApplication event received");
+    }
+
+    public void AdMobEnableRewardedVideoAd()
+    {
+        AdRequest request = new AdRequest.Builder().Build();
+        _adMobRewardedVideoAd.LoadAd(request, AdMobRewardedVideoAdId);
+    }
+
+    private void HandleInterstitialAdLoaded(object sender, EventArgs args)
+    {
+        print("HandleAdLoaded event received");
+        _adMobInterstitialAd.Show();
+    }
+
+    private void HandleInterstitialAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        print("HandleFailedToReceiveAd event received with message: " + args.Message);
+    }
+
+    private void HandleInterstitialAdOpened(object sender, EventArgs args)
+    {
+        print("HandleAdOpened event received");
+    }
+
+    private void HandleInterstitialAdClosed(object sender, EventArgs args)
+    {
+        print("HandleAdClosed event received");
+        //_adMobInterstitialAd.Destroy();
+    }
+
+    private void HandleInterstitialAdLeavingApplication(object sender, EventArgs args)
+    {
+        print("HandleAdLeavingApplication event received");
+        //_adMobInterstitialAd.Destroy();
+    }
+
+    public void AdMobEnableInterstitialAd()
+    {
+        _adMobInterstitialAd = new InterstitialAd(AdMobInterstitialAdId);
+        AdRequest request = new AdRequest.Builder().Build();
+        _adMobInterstitialAd.LoadAd(request);
+    }
+
+    public void AdMobEnableBannerAd()
+    {
+        _adMobBannerView = new BannerView(AdMobMenuBannerAdId, AdSize.Banner, AdPosition.Top);
+        AdRequest request = new AdRequest.Builder().Build();
+        _adMobBannerView.LoadAd(request);
+    }
+
+    public void UnityEnableRewardedVideoAd()
+    {
+        Advertisement.Load(UnityRewardedVideoPlacementId);
+        Advertisement.Show(UnityRewardedVideoPlacementId);
     }
     
-    public void EnableBannerAd()
+    public void UnityEnableInterstitialAd()
+    {
+        Advertisement.Load(UnityInterstitialPlacementId);
+        Advertisement.Show(UnityInterstitialPlacementId);
+    }
+    
+    public void UnityEnableBannerAd()
     {
         Advertisement.Banner.Load();
-        Advertisement.Banner.Show(MenuBannerPlacementId);
+        Advertisement.Banner.Show(UnityMenuBannerPlacementId);
     }
 
     private void UpdateTextField()
     {
-        if (textBox) { textBox.text = TextNumberOfCoins + "" + numberOfCoins + " coins"; }
-        else { Debug.LogError("Text box object not set"); }
+        if (textBox)
+        {
+            textBox.text = TextNumberOfCoins + "" + numberOfCoins + " coins and " + numberOfTokens + " tokens";
+        }
+        else
+        {
+            Debug.LogError("Text box object not set");
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        string gameId = isPlatformAndroid ? GameIdAndroid : GameIdApple;
+        string gameId = isPlatformAndroid ? UnityGameIdAndroid : UnityGameIdApple;
         Advertisement.AddListener(GetComponent<IUnityAdsListener>());
         Advertisement.Initialize(gameId, testMode, enablePerPlacementLoad);
         Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
         UpdateTextField();
+        _adMobRewardedVideoAd = RewardBasedVideoAd.Instance;
+        AddListeners();
+        MobileAds.Initialize(AdMobGameIdAndroid);
+    }
+
+    private void AddListeners()
+    {
+        _adMobRewardedVideoAd.OnAdLoaded += HandleRewardBasedVideoLoaded;
+        _adMobRewardedVideoAd.OnAdFailedToLoad += HandleRewardBasedVideoFailedToLoad;
+        _adMobRewardedVideoAd.OnAdOpening += HandleRewardBasedVideoOpened;
+        _adMobRewardedVideoAd.OnAdStarted += HandleRewardBasedVideoStarted;
+        _adMobRewardedVideoAd.OnAdRewarded += HandleRewardBasedVideoRewarded;
+        _adMobRewardedVideoAd.OnAdClosed += HandleRewardBasedVideoClosed;
+        _adMobRewardedVideoAd.OnAdLeavingApplication += HandleRewardBasedVideoLeftApplication;
+        
+        _adMobInterstitialAd.OnAdLoaded += HandleInterstitialAdLoaded;
+        _adMobInterstitialAd.OnAdFailedToLoad += HandleInterstitialAdFailedToLoad;
+        _adMobInterstitialAd.OnAdOpening += HandleInterstitialAdOpened;
+        _adMobInterstitialAd.OnAdClosed += HandleInterstitialAdClosed;
+        _adMobInterstitialAd.OnAdLeavingApplication += HandleInterstitialAdLeavingApplication;
     }
 
     private void Update()
     {
         UpdateTextField();
+        
+        if(_adMobInterstitialAd.IsLoaded())
+            _adMobInterstitialAd.Show();
     }
 }
